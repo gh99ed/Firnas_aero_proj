c800fed5d39033d3a7ca61c733f76cb25553fb5c
gh99ed <86988326+gh99ed@users.noreply.github.com>
2023-09-12 07:22:04 +0300

    Update flaskAPI.py

--- flaskAPI.py
+++ flaskAPI.py
@@ -56,7 +56,7 @@ def serve():
     return send_from_directory(app.static_folder, 'index.html') 
 
 if __name__ == '__main__':
-    app.run(debug="False",host='0.0.0.0')
+    app.run(host='127.0.0.1', port=5000)
 
 
 
